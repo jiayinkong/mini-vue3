ac47a177cb831abd16b1012a96708f294ea852cd
jiayinkong <ry1n.kong@gmail.com>
2023-10-01 21:15:11 +0800

    feat: 实现effect返回runner

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -8,11 +8,28 @@ describe('effect', () => {
     });
     let nextAge;
 
-    effect(() => nextAge = reactiveObj.age + 1);
+    effect(() => {
+      nextAge = reactiveObj.age + 1
+    });
     expect(nextAge).toBe(11);
 
     // 更新
     reactiveObj.age += 1; 
     expect(nextAge).toBe(12)
+  });
+
+
+  it('should return runner when call effect', () => {
+    let foo = 1;
+    const runner = effect(() => {
+      foo += 1;
+      return 'runner';
+    });
+
+    expect(foo).toBe(2);
+
+    const r = runner();
+    expect(foo).toBe(3);
+    expect(r).toBe('runner');
   })
 })
