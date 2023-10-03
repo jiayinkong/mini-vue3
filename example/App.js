3012e9d5d33af0d7637d5ab6a54ecaae99560eb6
jiayinkong <ry1n.kong@gmail.com>
2023-10-03 23:01:35 +0800

    feat: 实现 processElement

--- example/App.js
+++ example/App.js
@@ -3,7 +3,10 @@ import { h } from '../lib/guide-mini-vue.esm.js';
 export const App = {
   name: 'App',
   render() {
-    return h('div', 'Hello' + this.msg);
+    return h('div', { id: 'root' }, [
+      h('p', { class: 'red' }, 'Hello'),
+      h('p', { class: 'blue' }, 'Mini-vue')
+    ]);
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -8,10 +8,46 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  // 处理组件
-  processComponent(vnode, container);
+  if(typeof vnode.type === 'object') {
+    // 处理组件
+    processComponent(vnode, container);
+  } else {
+    // 处理 Element
+    processElement(vnode, container);
+  }
+}
 
-  // 处理Element
+function processElement(vnode, container) {
+  // 1. 创建标签
+  const el = document.createElement(vnode.type);
+
+  // 2. 处理 标签内容
+  const { children } = vnode;
+  // children 是字符串
+  if(typeof children === 'string') {
+    el.textContent = children; 
+  // children 是数组
+  } else if(Array.isArray(children)) {
+    mountChildren(vnode, el);
+  }
+
+  const { props } = vnode;
+
+  // 3. 处理标签的 props 属性
+  // 循环 props
+  for(const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  // 4. 把 el 添加到 container
+  container.append(el);
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach(v => {
+    patch(v, container);
+  });
 }
 
 function processComponent(vnode, container) {
