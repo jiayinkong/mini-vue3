7bbef89538a8174429078a14a2465a85161eedb1
jiayinkong <ry1n.kong@gmail.com>
2023-10-04 22:23:28 +0800

    feat: 实现 shapeFlags

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,19 +1,22 @@
+import { isObject } from '../shared/index';
 import { 
   createComponentInstance, 
   setupComponent 
 } from './component';
+import { ShapeFlags } from './shapeFlags';
 
 export function render(vnode, container) {
   patch(vnode, container);
 }
 
 function patch(vnode, container) {
-  if(typeof vnode.type === 'object') {
-    // 处理组件
-    processComponent(vnode, container);
-  } else {
+  const { shapeFlags } = vnode;
+  if(shapeFlags & ShapeFlags.ELEMENT) {
     // 处理 Element
     processElement(vnode, container);
+  } else {
+    // 处理组件
+    processComponent(vnode, container);
   }
 }
 
@@ -26,12 +29,12 @@ function mountElement(vnode, container) {
   const el = (vnode.el = document.createElement(vnode.type));
 
   // 2. 处理 标签内容
-  const { children } = vnode;
+  const { children, shapeFlags } = vnode;
   // children 是字符串
-  if(typeof children === 'string') {
+  if(shapeFlags & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children; 
   // children 是数组
-  } else if(Array.isArray(children)) {
+  } else if(shapeFlags & ShapeFlags.ARRAY_CHILDREN) {
     mountChildren(vnode, el);
   }
 
