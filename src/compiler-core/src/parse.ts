f53fc7d5eba68f1ac3300adfc2b5ec83aba3bd96
jiayinkong <ry1n.kong@gmail.com>
2023-10-09 20:28:10 +0800

    feat: 实现解析 element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,4 +1,4 @@
-import { NodeTypes } from './ast';
+import { NodeTypes, TagType } from './ast';
 
 export function baseParse(content: string) {
   const context = createParseContext(content);
@@ -9,12 +9,42 @@ function parseChildren(context) {
   const nodes: any[] = [];
  
   let node;
-  if(context.source.startsWith('{{')) {
+  const s = context.source;
+  if(s.startsWith('{{')) {
     node = parseInterpolation(context);
+  } else if(s[0] === '<') {
+    if(/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
 
   nodes.push(node);
   return nodes;
+}
+
+function parseElement(context) {
+  // 1.解析 tag
+  const element = parseTag(context, TagType.Start);
+
+  parseTag(context, TagType.End);
+
+  return element;
+}
+
+function parseTag(context, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+
+  const tag = match[1];
+  // 2. 删除处理完成的代码
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1); 
+
+  if (type === TagType.End) return;
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  }
 }
 
 function parseInterpolation(context) {
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -12,6 +12,17 @@ describe('Parse', () => {
           content: 'message',
         }
       })
+    });
+
+    describe('element', () => {
+      it('simple element div', () => {
+        const ast = baseParse('<div></div>');
+
+        expect(ast.children[0]).toStrictEqual({
+          type: NodeTypes.ELEMENT,
+          tag: 'div',
+        })
+      });
     })
   })
 })
