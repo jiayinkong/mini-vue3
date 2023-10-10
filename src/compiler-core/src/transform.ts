f49f056862ddb2865ce337475d02d53e29dc61b6
jiayinkong <ry1n.kong@gmail.com>
2023-10-10 21:47:35 +0800

    feat: 实现 transform 功能

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,36 @@
+export function transform(root, options) {
+  const context = createTransformText(root, options);
+
+  // 1.遍历 - 深度优先遍历
+  traverseNode(root, context);
+
+  // 2. 修改 text content
+}
+
+function createTransformText(root, options) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || [],
+  };
+
+  return context;
+}
+
+function traverseNode(node, context) {
+  console.log(node);
+
+  const nodeTransforms = context.nodeTransforms;
+  for(let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i];
+    transform(node);
+  }
+
+  const children = node.children;
+
+  if(children) {
+    for(let i = 0; i < children.length; i++) {
+      const node = children[i];
+      traverseNode(node, context);
+    }
+  }
+}
